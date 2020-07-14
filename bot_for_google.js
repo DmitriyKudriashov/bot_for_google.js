d1166d687a0fc0b6c451d2edf0f5f75143e86b4e
Кудряшов Дмитрий <67386647+DmitriyKudriashov@users.noreply.github.com>
2020-07-14 19:45:06 +0300

    Update bot_for_google.js
    откат назад без комментариев

--- bot_for_google.js
+++ bot_for_google.js
@@ -1,80 +1,54 @@
 // ==UserScript==
-// @name         bot_for_google
+// @name         botForGoogle
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
-// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
 
-
+let keywords = ["Гобой","Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель "];
+let keyword = keywords[getRandom(0,keywords.length)];
 let btnK = document.getElementsByName("btnK")[1];
 let links = document.links;
-let site = location.host;
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
-
-function writeKeyword(word){
-    let i=0;
-    let timerId = setInterval(()=>{
-        document.getElementsByName("q")[0].value+=word[i];
-        i++;
-        if(i==word.length){
-            clearInterval(timerId);
+@@ -26,17 +26,32 @@ function writeKeyWord(word){
+            clearInterval(timerID);
             btnK.click();
         }
-    },100);
+    },1000)
+    },100)
 }
 
-function getCookie(name) {
-  let matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-// Если btnK != undefined, значит мы на главной странице GOOGLE
-if (btnK != undefined){
-    let sites = {
-        "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"],
-        "crushdrummers.ru":["Шоу барабанщиков","Барабанное шоу","Заказать выступление барабанщиков","Crush барабанщики"]
-    };
-    let site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
-    let keywords = sites[site];
-    let keyword  = keywords[getRandom(0,keywords.length)];
-    document.cookie = "site="+site;
-    writeKeyword(keyword);
-}
-//Иначе если location.hostname == "www.google.com", то это страница с поисковой выдачей
+if (btnK != undefined)
+    writeKeyWord(keyword);
+else {
+let links = document.links;
 else if (location.hostname == "www.google.com"){
     let flag = true;
-    let site = getCookie("site");
-    for(let i=0; i<links.length; i++){
-        if(links[i].href.indexOf(site)!=-1){
-            let link = links[i];
-            flag=false;
-            setTimeout(()=>{link.click()},getRandom(1000,3000));
+    for (let i=0; i<links.length; i++){
+        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+            flag = false;
+            links[i].click();
             break;
         }
     }
     if (document.getElementsByClassName("YyVfkd")[0].textContent > 9){
-        flag = false;
-        location.href = "https://www.google.com/";
+    flag = false;
+    location.href = "https://www.google.com/";
     }
     if (flag) setTimeout(()=>{pnnext.click()},getRandom(3000,6000));
-}
-// Иначе мы находимся на искомом сайте
-else {
+}else{
     setInterval(()=>{
         if (getRandom(0,101)>=70) location.href = "https://www.google.com/";
         else{
-            let index = getRandom(0,links.length);
-            if(links[index].href.indexOf(site)!=-1)
+            let index = getRandom(0,links.length)
+            if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1)
                 links[index].click();
         }
-    },getRandom(2000,6000));
+    },getRandom(2000,4000));
 }
