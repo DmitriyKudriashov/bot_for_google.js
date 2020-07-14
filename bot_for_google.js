1edc962098614502979baf5d035e573715e1fc3d
Кудряшов Дмитрий <67386647+DmitriyKudriashov@users.noreply.github.com>
2020-07-14 19:18:07 +0300

    Update bot_for_google.js
    добавлены куки
    адаптирован под разные сайты

--- bot_for_google.js
+++ bot_for_google.js
@@ -1,57 +1,80 @@
 // ==UserScript==
-// @name         botForGoogle
+// @name         bot_for_google
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["Гобой","Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель "]; //объявляем массив для последующего набора в строке поиска
-let keyword = keywords[getRandom(0,keywords.length)]; //объявляем переменную путём случайного выбора из массива
-let btnK = document.getElementsByName("btnK")[1]; //объявляем переменную элемент главной кнопки поиска
-let links = document.links; //объявляем массив ссылок документа
-function getRandom(min,max){ // задаём функцию случайного значения от минимума до максимума
-    return Math.floor(Math.random()*(max-min)+min); //функция возвращает окрушленное к меньшему (Math.floor) значение от минимально положительного (не ноль) до максимально положительного
+
+let btnK = document.getElementsByName("btnK")[1];
+let links = document.links;
+let site = location.host;
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
 }
 
-function writeKeyWord(word){ //задаём функцию побуквенной печати слов
-    let i=0 //объявляем переменную 
-    let timerID = setInterval(()=>{ // объявляем идентификатор таймера для регулярно повторяющегося отсчета
-        document.getElementsByName("q")[0].value+=word[i]; //находим строку поиска и вносим значение переменной
-        i++ // инкремент
-        if (i==word.length){ //если переменная равна длинне слова ?? кстати я не понимаю от куда берется (word) значение
-            clearInterval(timerID); //остановить выполнение функции регулярного отсчета
-            btnK.click(); // нажать кнопку  поиска
+function writeKeyword(word){
+    let i=0;
+    let timerId = setInterval(()=>{
+        document.getElementsByName("q")[0].value+=word[i];
+        i++;
+        if(i==word.length){
+            clearInterval(timerId);
+            btnK.click();
         }
-    },100) // объявляем значение setInterval в миллисекундах
+    },100);
 }
 
-if (btnK != undefined) //если значение кнопки поиска не является неопределённым
-    writeKeyWord(keyword); //начинаем печатать побуквенно выбранное случайно слово
-else if (location.hostname == "www.google.com"){ //если главный домен гугл
-    let flag = true; //объявляем переменную флаг = верно
-    for (let i=0; i<links.length; i++){ //тогда цикл фор: переменная i меньше количества ссылок в инкременте 
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){ // если попалась ссылка сайта музыкалка
-            flag = false; //переменная флага меняет значение = неверно
-            links[i].click(); //клик на ссылку
-            break; //прерывание до окончания цикла
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// Если btnK != undefined, значит мы на главной странице GOOGLE
+if (btnK != undefined){
+    let sites = {
+        "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"],
+        "crushdrummers.ru":["Шоу барабанщиков","Барабанное шоу","Заказать выступление барабанщиков","Crush барабанщики"]
+    };
+    let site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
+    let keywords = sites[site];
+    let keyword  = keywords[getRandom(0,keywords.length)];
+    document.cookie = "site="+site;
+    writeKeyword(keyword);
+}
+//Иначе если location.hostname == "www.google.com", то это страница с поисковой выдачей
+else if (location.hostname == "www.google.com"){
+    let flag = true;
+    let site = getCookie("site");
+    for(let i=0; i<links.length; i++){
+        if(links[i].href.indexOf(site)!=-1){
+            let link = links[i];
+            flag=false;
+            setTimeout(()=>{link.click()},getRandom(1000,3000));
+            break;
         }
     }
-    if (document.getElementsByClassName("YyVfkd")[0].textContent > 9){ //если найденный по классу элемент документа первый из массива содержит текст больше цифры 9
-    flag = false; // то переменная флаг принимает значение = неверно
-    location.href = "https://www.google.com/"; //переходим на страницу хоста гугл
+    if (document.getElementsByClassName("YyVfkd")[0].textContent > 9){
+        flag = false;
+        location.href = "https://www.google.com/";
     }
-    if (flag) setTimeout(()=>{pnnext.click()},getRandom(3000,6000)); // если флаг верен, то спустя интервал времени (от 3х до 6ти секунд) клик на объект документа следстр
-}else{ //либо
-    setInterval(()=>{ //спустя повторяющийся отрезок времени
-        if (getRandom(0,101)>=70) location.href = "https://www.google.com/"; //с вероятностью 30% возврящаемся на гугл
-        else{ //либо
-            let index = getRandom(0,links.length) //случайность одна из ссылок на странице сайта на котором находимся
-            if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1) //проверяет наличие ссылки на музыкалку, не допуская другие ссылки (конструкциядвойного отрицания !=-1)
-                links[index].click(); // тогда клик
+    if (flag) setTimeout(()=>{pnnext.click()},getRandom(3000,6000));
+}
+// Иначе мы находимся на искомом сайте
+else {
+    setInterval(()=>{
+        if (getRandom(0,101)>=70) location.href = "https://www.google.com/";
+        else{
+            let index = getRandom(0,links.length);
+            if(links[index].href.indexOf(site)!=-1)
+                links[index].click();
         }
-    },getRandom(2000,4000)); //выбранный случайно от 2х до 4х секунд
+    },getRandom(2000,6000));
 }
